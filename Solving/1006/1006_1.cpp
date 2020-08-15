1f211757709381def28abf339de44f9ea50c1d8b
Seohyun Lee <04seohyun@gmail.com>
2020-08-16 01:09:29 +0900

    Sync

---
+++ Solving/1006/1006_1.cpp
@@ -0,0 +1,13 @@
+//2020 SEORII®. All right reserved.
+//Code created on 8/14/2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
